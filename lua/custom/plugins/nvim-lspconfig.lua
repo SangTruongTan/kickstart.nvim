5629790d8a6d01efb196bbe51e29fddca88f643a
Sang Tan Truong <sang.truong@outlook.com.vn>
2025-07-01 09:37:10 +0700

    Disable the vim-sleuth extension

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -20,5 +20,16 @@ return {
         vim.bo.cinoptions = ':0,l1,t0,g0,(0'
       end,
     })
+
+    -- Force tabs (no expandtab) for Makefiles
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'make' },
+      callback = function()
+        vim.bo.tabstop = 4
+        vim.bo.shiftwidth = 4
+        vim.bo.expandtab = false -- Use hard tabs
+        vim.bo.softtabstop = 0 -- Disable soft tabs
+      end,
+    })
   end,
 }
